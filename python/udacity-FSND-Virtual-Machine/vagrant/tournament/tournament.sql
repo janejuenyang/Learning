438d05d3a8add372e0625e4dda9b1aba243f5954
Jane Yang <jane.juen.yang@gmail.com>
2017-10-15 22:04:37 -0500

    First draft of database complete

--- python/udacity-FSND-Virtual-Machine/vagrant/tournament/tournament.sql
+++ python/udacity-FSND-Virtual-Machine/vagrant/tournament/tournament.sql
@@ -1,9 +1,48 @@
 -- Table definitions for the tournament project.
---
--- Put your SQL 'create table' statements in this file; also 'create view'
--- statements if you choose to use it.
---
--- You can write comments in this file by starting them with two dashes, like
--- these lines here.
 
+-- check if database 'tournament' already exists; if so, delete and create an (empty) database
+DROP DATABASE IF EXISTS tournament;
+CREATE DATABASE tournament;
 
+-- psql code to connect to the database
+\c tournament
+
+-- create a table to keep track of player identification information
+CREATE TABLE players
+(
+    player_id serial primary key
+  , name varchar(100) -- assumes names are <100 characters long...
+)
+;
+
+-- create a table to track match outcomes
+-- note: a match must be between two different players
+CREATE TABLE matches
+(
+    match_id serial primary key
+  , round integer
+  , player_1 integer references players(player_id)
+  , player_2 integer references players(player_id) check player_2 <> player_1
+  , winner integer check (
+         winner = player_1
+      or winner = player_2
+      or winner is NULL
+    )
+)
+;
+
+-- create a flat table that makes it easy to return a list of players
+-- and their tournament record
+CREATE VIEW scoreboard AS
+  SELECT
+      player_id
+    , name
+    -- calculate number of wins per player
+    , count(*) FILTER (WHERE winner = player_id) AS wins
+    -- calculate number of matches played per player
+    , count(player_1) FILTER (WHERE player_1 = player_id) +
+      count(player_2) FILTER (WHERE player_1 = player_id)
+        AS matches
+  FROM players INNER JOIN matches ON matches.winner = players.player_id
+  ORDER BY wins DESC
+;

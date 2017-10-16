2fb61e4a5bff296abb4811879f5c1ad74490b94b
Jane Yang <jane.juen.yang@gmail.com>
2017-10-15 22:22:57 -0500

    Expand players to hold wins and matches counts

--- python/udacity-FSND-Virtual-Machine/vagrant/tournament/tournament.sql
+++ python/udacity-FSND-Virtual-Machine/vagrant/tournament/tournament.sql
@@ -12,11 +12,14 @@ CREATE TABLE players
 (
     player_id serial primary key
   , name varchar(100) -- assumes names are <100 characters long...
+  , wins -- +1 for each win
+  , matches -- +1 for each match they compete in
 )
 ;
 
 -- create a table to track match outcomes
 -- note: a match must be between two different players
+-- note: we assume there are no draws in a match
 CREATE TABLE matches
 (
     match_id serial primary key
@@ -26,11 +29,12 @@ CREATE TABLE matches
   , winner integer check (
          winner = player_1
       or winner = player_2
-      or winner is NULL
+      or winner is NULL -- only until match is over; maybe drop?
     )
 )
 ;
 
+/*
 -- create a flat table that makes it easy to return a list of players
 -- and their tournament record
 CREATE VIEW scoreboard AS
@@ -46,3 +50,4 @@ CREATE VIEW scoreboard AS
   FROM players INNER JOIN matches ON matches.winner = players.player_id
   ORDER BY wins DESC
 ;
+*/
